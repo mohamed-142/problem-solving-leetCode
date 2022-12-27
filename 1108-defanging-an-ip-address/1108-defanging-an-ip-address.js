0ad3ff9a05821762540ce0a9166dac9ab0cc2e76
Mohamed Mostafa <39238396+domyaty@users.noreply.github.com>
2022-12-27 03:03:52 +0200

    Time: 71 ms (72.76%), Space: 41.9 MB (44.79%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -2,15 +2,4 @@
  * @param {string} address
  * @return {string}
  */
-var defangIPaddr = function(address) {
-    var result="";
-    
-    for(char of address) {
-        if(char === ".")
-            result += "[.]";
-        else
-            result += char;
-    }
-    
-    return result;    
-};
+var defangIPaddr = address => address.replaceAll('.', '[.]');
