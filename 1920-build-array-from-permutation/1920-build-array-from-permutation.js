152b9383fb07d2050d062329ee339f067ee63036
Mohamed Mostafa <39238396+domyaty@users.noreply.github.com>
2022-12-27 02:40:40 +0200

    Time: 82 ms (91.61%), Space: 46.3 MB (37.27%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -4,7 +4,6 @@
  */
 var buildArray = function(nums) {
     let ans = [];
-    for(let i = 0; i < nums.length; i++)
-        ans.push(nums[nums[i]]);
+    nums.forEach(e => ans.push(nums[e]));
     return ans;
 };
