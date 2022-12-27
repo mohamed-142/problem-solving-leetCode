7c197253b9e4ffb06430994a62cd447427eb5031
Mohamed Mostafa <39238396+domyaty@users.noreply.github.com>
2022-12-27 02:06:35 +0200

    Time: 66 ms (84.92%), Space: 42.3 MB (20.51%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -2,6 +2,4 @@
  * @param {number} celsius
  * @return {number[]}
  */
-var convertTemperature = function(celsius) {
-    return [celsius + 273.15, celsius * 1.80 + 32.00]
-};
+var convertTemperature = celsius => [celsius + 273.15, celsius * 1.80 + 32.00]
