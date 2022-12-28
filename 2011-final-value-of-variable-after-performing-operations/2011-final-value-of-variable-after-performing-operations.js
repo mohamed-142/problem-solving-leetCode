a78e3dbe74583032f6c63f15d484da2ce28599e9
Mohamed Mostafa <39238396+domyaty@users.noreply.github.com>
2022-12-28 07:19:38 +0200

    Time: 58 ms (98.66%), Space: 42 MB (91.02%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    let p = operations.filter((op)=> op.includes('+')).length;
-    let m = operations.length - p;
-    return p + (m*-1)
+    let x = 0;
+    operations.map(e => e.includes("+") ? x++ : x--);
+    return x;
 };
