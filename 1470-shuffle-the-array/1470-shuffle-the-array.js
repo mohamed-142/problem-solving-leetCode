1624c55404b9030aa31955a4a24da0e3edaabba8
Mohamed Mostafa <39238396+domyaty@users.noreply.github.com>
2022-12-28 07:53:06 +0200

    Time: 79 ms (81.68%), Space: 43.8 MB (93.73%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,12 +4,21 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let ans = [];
-    nums.map((e, i, arr) => {
-        if(i < n) {
-            ans.push(arr[i]);
-            ans.push(arr[i+n]);
-        }
-    });
-    return ans;
+    let i = n - 1
+    for (let j = nums.length - 1; j >= n; j--) {
+        nums[j] <<= 10
+        nums[j] |= nums[i]
+        i--
+    }
+    
+    i = 0
+    for (let j = n; j < nums.length; j++) {
+        const num1 = nums[j] & 1023
+        const num2 = nums[j] >> 10
+        nums[i] = num1
+        nums[i + 1] = num2
+        i += 2    
+    }
+    
+    return nums
 };
