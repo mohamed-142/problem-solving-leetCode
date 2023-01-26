e569c25ff2fc81265f2f1929b0ddb5ccb5387da2
Mohamed Mostafa <39238396+mohamed-142@users.noreply.github.com>
2023-01-26 07:14:21 +0200

    Time: 2 ms (23.38%), Space: 41.6 MB (91.45%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -2,7 +2,7 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
         int max = 0;
         for(int[] customer: accounts) {
-            max = Arrays.stream(customer).sum() > max ? Arrays.stream(customer).sum() : max;   
+            max = Math.max(max, Arrays.stream(customer).sum());   
         }
         return max;
     }
