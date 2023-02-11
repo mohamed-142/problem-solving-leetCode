d146caa0809047f02979be09f794eed88146009b
Mohamed Mostafa <39238396+mohamed-142@users.noreply.github.com>
2023-02-11 04:20:11 +0200

    Time: 1 ms (81.11%), Space: 40.6 MB (52.88%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,21 +1,29 @@
 class Solution {
     public String interpret(String command) {
+        
         int i = 0;
         String result = "";
+        
         while (i < command.length()) {
+            
             if (command.substring(i, i+1).equals("G")) {
                 result += "G";
                 i++;
             }
+            
             else if (command.substring(i, i+2).equals("()")) {
                 result += "o";
                 i += 2;
             }
+            
             else if (command.substring(i, i+4).equals("(al)")) {
                 result += "al";
                 i += 4;
             }
+            
         }
+        
         return result;
+        
     }
 }
