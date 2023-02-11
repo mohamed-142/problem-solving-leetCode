e99ee67cac248d7e9bfe518a547f6678e1dfd64a
Mohamed Mostafa <39238396+mohamed-142@users.noreply.github.com>
2023-02-11 04:12:40 +0200

    Time: 1 ms (81.11%), Space: 40.6 MB (52.88%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,23 +1,18 @@
 class Solution {
     public String interpret(String command) {
         int i = 0;
-        Map<String, String> values = Map.of(
-            "G", "G",
-            "()", "o",
-            "(al)", "al"
-        );
         String result = "";
-        while (command != "" && i < command.length()) {
-            if (values.containsKey(command.substring(i, i+1))) {
-                result += values.get(command.substring(i, i+1));
+        while (i < command.length()) {
+            if (command.substring(i, i+1).equals("G")) {
+                result += "G";
                 i++;
             }
-            else if (values.containsKey(command.substring(i, i+2))) {
-                result += values.get(command.substring(i, i+2));
+            else if (command.substring(i, i+2).equals("()")) {
+                result += "o";
                 i += 2;
             }
-            else if (values.containsKey(command.substring(i, i+4))) {
-                result += values.get(command.substring(i, i+4));
+            else if (command.substring(i, i+4).equals("(al)")) {
+                result += "al";
                 i += 4;
             }
         }
