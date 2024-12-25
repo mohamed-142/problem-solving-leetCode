d39ab9354d4ceebd659e822aac478d626265611e
Mohamed Mostafa <39238396+mohamed-142@users.noreply.github.com>
2024-12-26 00:24:09 +0200

    Time: 1 ms (83.55%), Space: 41.5 MB (83.01%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,15 +1,15 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int i = 0;
         for(; i < word1.length() || i < word2.length(); i++) {
             if(i < word1.length()) {
-                result += word1.charAt(i);
+                result.append(word1.charAt(i));
             } 
             if (i < word2.length()) {
-                result += word2.charAt(i);
+                result.append(word2.charAt(i));
             }
         }
-        return result;
+        return result.toString();
     }
 }
